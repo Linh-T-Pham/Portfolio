359e353d0283b1875dabea1835d569c4d50bf645
linh <thuytpham9@gmail.com>
2020-03-29 19:12:17 -0700

    Add colors and styles

--- my-portfolio/src/components/landingpage.js
+++ my-portfolio/src/components/landingpage.js
@@ -1,10 +1,25 @@
 import React, { Component} from 'react';
+import { Grid, Cell } from 'react-mdl';
 
 class Landing extends Component {
     render() {
         return(
-            <div>
-                <h1>Landing Page</h1>   
+            <div style={{width: '100%', margin: 'auto' }}>
+                <Grid className="landing-grid">
+                    <Cell col={12}>
+                        <img
+                            src='/Me with Bo.png' width="200" height="200" alt="image"
+                            className="my-image"
+                        />
+                        <div className="banner-text">
+                            <h1>Full Stack Software Engineer</h1>
+                    <p>Python | Javascript | React | SQL | PostgresSQL | R | Boostrap | HTML/CSS</p>
+
+                    
+                    <hr/> 
+                    </div>
+                    </Cell>
+                </Grid>               
             </div>
         )
     }
